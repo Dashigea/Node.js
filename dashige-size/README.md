9eaa93b3ee85145f5b77717fe695021da108bbf1
dashige <541841650@qq.com>
2018-07-09 22:05:51 +0800

    nodejs npm size module

---
+++ dashige-size/README.md
@@ -0,0 +1,10 @@
+## 我就是我,颜色不一样的花火
+    引入包后可以使用
+##    加法
+    dashige-size.add(a,b)
+##    减法
+    dashige-size.sub(a,b)
+##    乘法
+    dashige-size.mult(a,b)
+##    除法
+    dashige-size.div(a,b)
---
+++ dashige-size/lib/index.js
@@ -0,0 +1,20 @@
+const add = (a,b)=>{
+    return a+b
+}
+const sub = (a,b)=>{
+    return a-b
+}
+const mult = (a,b)=>{
+    return a*b
+}
+const div = (a,b)=>{
+    return a/b
+}
+ 
+// 暴露出去
+ module.exports = {
+     add,
+     sub,
+     mult,
+     div
+ }
